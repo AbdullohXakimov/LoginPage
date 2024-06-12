3f5ff17c8706dd1b5a19114b631a81b8e3afa63a
AbdullohXakimov <Abdullohh745@gmail.com>
2024-06-12 11:46:00 +0500

    lets start

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        primary: "#1C1E53",
+      },
+    },
+  },
+  plugins: [],
+};
